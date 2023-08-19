1b4e65055f8a0169f2dec1239ba340e2b80d1540
dragoncheeze <colby.braden@gmail.com>
2023-08-19 07:28:30 -0500

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 alias v="nvim"
+alias sway="dbus-run-session sway"
 #alias sway="dbus-run-session sway --unsupported-gpu -D noscanout"
 alias reboot="loginctl reboot"
 alias shutdown="loginctl poweroff"
