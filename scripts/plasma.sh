58b5a722200a941348ecf53f4d95da6d19f44b93
Colby <colby.braden@gmail.com>
2023-11-30 08:57:26 -0600

    added plasma install script for default intall

---
+++ scripts/plasma.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Default programs
+sudo xbps-install linux-headers base-devel xmirror cronie chrony pulseaudio libpulseaudio-32bit wl-clipboard freetype freetype-32bit fontconfig-devel libX11-devel libXft-devel libXinerama-devel elogind eudev nss-mdns xorg xtools vkd3d vkd3d-32bit vulkan-loader vulkan-loader-32bit cups ipp-usb cups-filters ghostscript xorg-apps xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils python fuse wine rsync alacritty firefox ttf-ubuntu-font-family gnutls-32bit libldap-32bit libgpg-error-32bit sqlite-32bit libpulseaudio-32bit alsa-plugins-32bit pulsemixer gvfs gamemode curl wget alsa-utils mesa-dri-32bit neofetch htop kde5 kde5-baseapps skanlite kdegraphics-thumbnailers ffmpegthumbs gwenview ark okular -yy
