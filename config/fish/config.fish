f5e8a761d0382af5ad9bc217ac333a53ab4765d6
Colby <colby.braden@gmail.com>
2023-11-30 09:01:20 -0600

    added plasma to alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 alias v="nvim"
+alias kde="dbus-run-session startplasma-wayland"
 alias sway="dbus-run-session sway"
 #alias sway="dbus-run-session sway --unsupported-gpu -D noscanout"
 alias reboot="loginctl reboot"
