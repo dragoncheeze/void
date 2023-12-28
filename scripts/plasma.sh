f56169c79d14f78c4d44604f59752952ea6a8274
Colby <colby.braden@gmail.com>
2023-12-28 11:53:19 -0600

    updated install scripts

--- scripts/plasma.sh
+++ scripts/plasma.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 # Default programs
-sudo xbps-install linux-headers base-devel xmirror cronie chrony pulseaudio libpulseaudio-32bit wl-clipboard freetype freetype-32bit fontconfig-devel libX11-devel libXft-devel libXinerama-devel elogind eudev nss-mdns xorg xtools vkd3d vkd3d-32bit vulkan-loader vulkan-loader-32bit cups ipp-usb cups-filters ghostscript xorg-apps xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils python fuse wine rsync alacritty firefox ttf-ubuntu-font-family gnutls-32bit libldap-32bit libgpg-error-32bit sqlite-32bit libpulseaudio-32bit alsa-plugins-32bit pulsemixer gvfs gamemode curl wget alsa-utils mesa-dri-32bit neofetch htop kde5 kde5-baseapps skanlite kdegraphics-thumbnailers ffmpegthumbs gwenview ark okular -yy
+sudo xbps-install linux-headers base-devel xmirror cronie chrony pulseaudio \
+    libpulseaudio-32bit wl-clipboard freetype freetype-32bit fontconfig-devel \
+    libX11-devel libXft-devel libXinerama-devel elogind eudev nss-mdns xorg \
+    xtools vkd3d vkd3d-32bit vulkan-loader vulkan-loader-32bit cups ipp-usb \
+    cups-filters ghostscript xorg-apps xdg-desktop-portal xdg-desktop-portal-gtk \
+    xdg-user-dirs xdg-user-dirs-gtk xdg-utils python fuse wine wine-32bit rsync \
+    ttf-ubuntu-font-family gnutls-32bit libldap-32bit libgpg-error-32bit firefox \
+    sqlite-32bit libpulseaudio-32bit alsa-plugins-32bit pulsemixer gvfs gamemode \
+    curl wget alsa-utils mesa-dri-32bit neofetch htop kde5 kde5-baseapps \
+    kdegraphics-thumbnailers ffmpegthumbs gwenview ark okular skanlite -yy
 # Lutris Gaming
-sudo xbps-install lutris libunwind libunwind-32bit mesa-dri mesa-vaapi mesa-vdpau mesa-vulkan-radeon mesa-vulkan-radeon-32bit -yy
+sudo xbps-install lutris libunwind libunwind-32bit mesa-dri mesa-vaapi \
+    mesa-vdpau mesa-vulkan-radeon mesa-vulkan-radeon-32bit -yy
--- scripts/void-defaults.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Default programs
-sudo xbps-install linux-headers base-devel xmirror gnome-keyring cronie chrony pulseaudio libpulseaudio-32bit xclip freetype freetype-32bit fontconfig-devel libX11-devel libXft-devel libXinerama-devel elogind eudev nss-mdns xorg xtools vkd3d vkd3d-32bit vulkan-loader vulkan-loader-32bit cups ipp-usb cups-filters ghostscript xorg-apps xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils python fuse dunst wine wine-32bit rsync alacritty firefox dmenu gtk-engine-murrine ttf-ubuntu-font-family gnutls-32bit libldap-32bit libgpg-error-32bit sqlite-32bit libpulseaudio-32bit alsa-plugins-32bit picom lxappearance pavucontrol pulsemixer papirus-icon-theme \
-Thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler gvfs xarchiver rofi galculator-gtk3 geany gamemode lxsession curl wget cargo sxiv scrot nitrogen alsa-utils mesa-dri-32bit arc-theme neofetch htop mpv \
-bspwm sxhkd polybar autotiling -yy
