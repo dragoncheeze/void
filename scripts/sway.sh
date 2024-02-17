e4e99adf90ae81243e37edd88c05aae55970ec42
Colby <colby.braden@gmail.com>
2024-02-17 08:40:39 -0600

    updated sway install script

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -3,12 +3,11 @@
 sudo xbps-install linux-headers playerctl base-devel \
     gnome-keyring xdg-desktop-portal-gtk xmirror cronie \
     chrony pulseaudio libpulseaudio-32bit wl-clipboard \
-    freetype freetype-32bit fontconfig-devel libX11-devel \
-    libXft-devel libXinerama-devel elogind nss-mdns xorg \
-    xtools vulkan-loader vulkan-loader-32bit \
-    cups ipp-usb cups-filters ghostscript xorg-apps xdg-desktop-portal \
+    freetype freetype-32bit elogind nss-mdns \
+    xtools vulkan-loader vulkan-loader-32bit xorg-server-xwayland\
+    cups ipp-usb cups-filters ghostscript xdg-desktop-portal \
     xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils \
-    python fuse dunst wine wine-32bit rsync foot alacritty firefox \
+    python fuse dunst wine wine-32bit rsync foot firefox \
     dmenu gtk-engine-murrine ttf-ubuntu-font-family gnutls-32bit \
     libldap-32bit libgpg-error-32bit sqlite-32bit libpulseaudio-32bit \
     alsa-plugins-32bit nwg-look pavucontrol pulsemixer papirus-icon-theme \
@@ -17,6 +16,6 @@ sudo xbps-install linux-headers playerctl base-devel \
     curl wget alsa-utils mesa-dri-32bit arc-theme neofetch htop imv i3status \
     sway seatd autotiling -yy &&
 # Lutris Gaming
-sudo xbps-install libunwind libunwind-32bit mesa-dri mesa-vaapi \
+sudo xbps-install lutris libunwind libunwind-32bit mesa-dri mesa-vaapi \
     mesa-vdpau mesa-vulkan-radeon vulkan-loader vulkan-loader-32bit \
     mesa-vulkan-radeon-32bit -yy
