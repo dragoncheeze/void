08bc4f907c46ca1920fbf116fc08d5b27046b1c8
Colby <colby.braden@gmail.com>
2024-02-25 08:46:29 -0600

    updated brave install script

--- scripts/brave-deb.sh
+++ scripts/brave-deb.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+## variables
 version=1.62.165
 name=brave-bin
-wget https://github.com/brave/brave-browser/releases/download/v$version/brave-browser_${version}_amd64.deb
+## Download latest .deb
+wget https://github.com/brave/brave-browser/releases/download/v$version/brave-browser_${version}_amd64.deb &&
+	bsdtar -xf brave-browser_${version}_amd64.deb &&
+	sudo bsdtar -xf data.tar.xz -C / &&
+# cleanup
+	rm brave-browser_${version}_amd64.deb &&
+	rm control.tar.xz &&
+	rm data.tar.xz &&
+	rm debian-binary &&
+	clear &&
+# completed message
+echo Brave ${version} Installed
