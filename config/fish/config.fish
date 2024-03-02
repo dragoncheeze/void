6ab546abea0a1bbf4fc9b5677c4e1df98e716dcf
Colby <colby.braden@gmail.com>
2024-03-02 07:59:07 -0600

    updated configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 alias v="vim"
 #alias kde="dbus-run-session startplasma-wayland"
-alias sway="dbus-run-session sway"
+alias sway="kill pipewire && dbus-run-session sway"
 #alias sway="dbus-run-session sway --unsupported-gpu -D noscanout"
 alias reboot="loginctl reboot"
 alias shutdown="loginctl poweroff"
