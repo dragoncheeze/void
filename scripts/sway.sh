ce1f43845356299a757ed76be5af2b825e4ac16f
Colby <colby.braden@gmail.com>
2024-06-12 23:30:15 -0500

    updated sway configs

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -13,9 +13,9 @@ sudo xbps-install linux-headers playerctl base-devel \
     alsa-plugins-32bit nwg-look pavucontrol pulsemixer papirus-icon-theme \
     Thunar file-roller thunar-archive-plugin thunar-media-tags-plugin \
     thunar-volman tumbler gvfs rofi galculator-gtk3 geany gamemode lxsession \
-    curl wget alsa-utils mesa-dri-32bit arc-theme neofetch htop imv i3status \
+    curl wget alsa-utils mesa-dri-32bit arc-theme fastfetch htop imv i3status \
     sway seatd autotiling lxappearance -yy &&
 # Lutris Gaming
-sudo xbps-install lutris libunwind libunwind-32bit mesa-dri mesa-vaapi \
+sudo xbps-install libunwind libunwind-32bit mesa-dri mesa-vaapi \
     mesa-vdpau mesa-vulkan-radeon vulkan-loader vulkan-loader-32bit \
     mesa-vulkan-radeon-32bit -yy
